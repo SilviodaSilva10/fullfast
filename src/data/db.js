d1aa5caec7a04d7c93301541c2d90473b96b5518
SilviodaSilva10 <silviodasilva@yandex.com>
2026-04-18 22:20:59 +0100

    db

--- src/data/db.js
+++ src/data/db.js
@@ -2,12 +2,16 @@ import mongoose from 'mongoose'
 
 console.log('Conectando ao MongoDB...') 
 
-const connectDBatlas= ()=>{
+const connectDBatlas = () => {
     mongoose.connect(process.env.MONGO_URI)
-    .then( () => console.log('MongoDBAtlas conectado') )
-    .catch( (err) => console.error('Erro ao conectar ao MongoDBAtlas:', err) )
+    .then(() => console.log('MongoDBAtlas conectado'))
+    .catch((err) => {
+        // Criando um erro personalizado com mais contexto
+        const erroPersonalizado = new Error("Falha Crítica: Não foi possível alcançar o cluster do Atlas.");
+        console.error(erroPersonalizado.message);
+        console.error("Detalhes técnicos:", err.message);
+    });
 }
-
 let connectDB = connectDBatlas
 
 export default connectDB
