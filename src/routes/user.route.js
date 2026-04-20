6561987fec1916bfac287af70e657ae4332e3a84
SilviodaSilva10 <silviodasilva@yandex.com>
2026-04-20 19:55:20 +0100

    user crotes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,9 +6,9 @@ const router =Router()
 
 import {validId,validUser} from  '../middlewares/global.middlewares.js'
 
-router.post('/', userController.cadastro)
+router.post('/create', userController.cadastro)
 router.get('/',userController.findAll)
-router.get('/:id', validId,validUser, userController.findUserById)
+router.get('/findById/:id', validId,validUser, userController.findUserById)
 router.patch('/update/:id',  validId,validUser, userController.update)
 router.delete('/:id', validId,validUser, userController.deleteUser)
 
